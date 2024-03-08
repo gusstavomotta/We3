3be3e973ee42c3e40b3d40eab4ee25be2f604ccf
Rafazanella01 <rafazanella2004@gmail.com>
2024-03-08 11:46:00 -0300

    Add Apis and Update main

--- Apis/Api_Produto.php
+++ Apis/Api_Produto.php
@@ -1,2 +1,24 @@
 <?php
 
+function WebServiceExec($params, $data){ 
+	
+    $produto = Db::Read()->clear()
+        ->select([
+             'p.idproduto',
+             'p.dscproduto',
+             'p.preco'
+        ])
+        ->from('pdv_produto p')
+        ->whereAND([
+             'p.preco' => 50
+        ])
+		->groupBy([1,2,3])
+        ->fetchAll();
+	
+    
+    if(empty($produto)){
+         return false;   
+    } 
+    
+    return $produto;	
+}
