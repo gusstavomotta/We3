93f651fabbb5d20f97f9cce24bc0947682066c58
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2024-03-11 08:31:35 -0300

    A

--- funcoes/login.php
+++ funcoes/login.php
@@ -1,5 +1,6 @@
 <?php 
 
+var_dump("TESTANDO TESTANDO, HTML MANDOU CERTO");
 session_start();
 
 $pessoa_api = Requisicao::requisicaoLogin('65948272788', 'jefersongvargas2009@hotmail.com');
--- main/index.php
+++
@@ -1,9 +0,0 @@
-
-<?php 
- 
-require __DIR__ . '/../classes/Requisicao.php';
-require __DIR__ . '/../classes/Carrinho.php';
-require __DIR__ . '/../classes/Usuario.php';
-require __DIR__ . '/../classes/Produto.php';
-
-
