5ddf67a2e4204e453bf3349401bb08bf0d9e55fe
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2024-03-14 11:13:23 -0300

    Update encerraSessao.php

--- views/encerraSessao.php
+++ views/encerraSessao.php
@@ -1,9 +1,11 @@
 <?php
 session_start();
+
 unset($_SESSION['produtos']);
 unset($_SESSION['cpf']);
 unset($_SESSION['email']);
 unset($_SESSION['nome']);
 unset($_SESSION['id']);
 sleep(2);
+
 header("Location: ../views/apaginaInicial.php");
