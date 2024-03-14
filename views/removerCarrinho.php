c89655ae1abced3935643b44d56ad67d8968732e
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2024-03-14 09:01:29 -0300

    Update removerCarrinho.php

--- views/removerCarrinho.php
+++ views/removerCarrinho.php
@@ -2,11 +2,11 @@
 require __DIR__ . "/../Controller/carrinhoController.php";
 
 session_start();
-$produtos = $_SESSION['produtos'];
 $id_produto = $_POST['produto'];
 
+$produtos = $_SESSION['produtos'];
+
 print($produtos);
-var_dump($id_produto);
 
 
 $_SESSION['produtos'] = remover_produto_do_carrinho($produtos, $id_produto);
